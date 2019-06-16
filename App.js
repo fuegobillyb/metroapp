70524e9894e87b319c9edca8ad814257b00ad44e
Aberham Haile <aberhamhaile@Aberhams-MacBook-Pro.local>
2019-06-16 17:26:53 -0400

    added availability/location tables to book/show

--- App.js
+++ App.js
@@ -1,236 +1,301 @@
+require('react-devtools-core').connectToDevTools({host: "http://192.168.0.10:8081"})
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { Button, StyleSheet, Platform, View, ActivityIndicator, FlatList, Text, Image, Alert, YellowBox } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
-import { SearchBar } from 'react-native-elements';
+import {
+    Button,
+    StyleSheet,
+    Platform,
+    View,
+    ActivityIndicator,
+    FlatList,
+    Text,
+    Image,
+    Alert,
+    ScrollView,
+    YellowBox
+} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation'; // Version can be specified in package.json
+import {SearchBar} from 'react-native-elements';
+import {Table, TableWrapper, Row, Rows, Col, Cols, Cell} from 'react-native-table-component';
+
 
 class Metro extends Component {
 
- constructor(props) {
+    constructor(props) {
 
-   super(props);
+        super(props);
 
-   this.state = {
-     search: '',
-     isLoading: true
+        this.state = {
+            search: '',
+            isLoading: true
 
-   }
+        }
 
-  this.arrayholder = [];
-
-
-   YellowBox.ignoreWarnings([
-    'Warning: componentWillMount is deprecated',
-    'Warning: componentWillReceiveProps is deprecated',
-  ]);
-
- }
+        this.arrayholder = [];
 
 
- FlatListItemSeparator = () => {
-   return (
-     <View
-       style={{
-         height: .5,
-         width: "100%",
-         backgroundColor: "#000",
-       }}
-     />
-   );
- }
+        YellowBox.ignoreWarnings([
+            'Warning: componentWillMount is deprecated',
+            'Warning: componentWillReceiveProps is deprecated',
+        ]);
 
-   componentDidMount(){
-     return fetch('http://192.168.0.10:4000/api/books')
-       .then((response) => response.json())
-       .then((responseJson) => {
+    }
 
-         this.setState({
-           isLoading: false,
-           dataSource: responseJson.books,
-         },
-          function() {
-            this.arrayholder = responseJson.books;
-          }
 
-         );
+    FlatListItemSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: .5,
+                    width: "100%",
+                    backgroundColor: "#000",
+                }}
+            />
+        );
+    }
 
-       })
-       .catch((error) =>{
-         console.error(error);
-       });
-   }
+    componentDidMount() {
+        return fetch('http://192.168.0.10:4000/api/books')
+            .then((response) => response.json())
+            .then((responseJson) => {
 
-     search = text => {
-       console.log(text);
-     };
-     clear = () => {
-       this.search.clear();
-     };
+                this.setState({
+                        isLoading: false,
+                        dataSource: responseJson.books,
+                    },
+                    function () {
+                        this.arrayholder = responseJson.books;
+                    }
+                );
 
-  SearchFilterFunction(text) {
-    //passing the inserted text in textinput
-    const newData = this.arrayholder.filter(function(item) {
-      //applying filter for the inserted text in search bar
-      const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
-      const textData = text.toUpperCase();
-      return itemData.indexOf(textData) > -1;
-    });
-    this.setState({
-      //setting the filtered newData on datasource
-      //After setting the data it will automatically re-render the view
-      dataSource: newData,
-      search:text,
-    });
-  }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 
-    ListViewItemSeparator = () => {
-      //Item sparator view
-      return (
-        <View
-          style={{
-            height: 0.3,
-            width: '90%',
-            backgroundColor: '#080808',
-          }}
-        />
-      );
+    search = text => {
+        console.log(text);
+    };
+    clear = () => {
+        this.search.clear();
     };
 
- render() {
+    SearchFilterFunction(text) {
+        //passing the inserted text in textinput
+        const newData = this.arrayholder.filter(function (item) {
+            //applying filter for the inserted text in search bar
+            const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+            const textData = text.toUpperCase();
+            return itemData.indexOf(textData) > -1;
+        });
+        this.setState({
+            //setting the filtered newData on datasource
+            //After setting the data it will automatically re-render the view
+            dataSource: newData,
+            search: text,
+        });
+    }
 
-   if (this.state.isLoading) {
-     return (
+    ListViewItemSeparator = () => {
+        //Item sparator view
+        return (
+            <View
+                style={{
+                    height: 0.3,
+                    width: '90%',
+                    backgroundColor: '#080808',
+                }}
+            />
+        );
+    };
 
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    render() {
 
-         <ActivityIndicator size="large" />
+        if (this.state.isLoading) {
+            return (
 
-       </View>
+                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 
-     );
+                    <ActivityIndicator size="large"/>
 
-   }
+                </View>
 
-   return (
+            );
 
-     <View style={styles.MainContainer}>
+        }
 
-             <SearchBar
-               round
-               searchIcon={{ size: 24 }}
-               onChangeText={text => this.SearchFilterFunction(text)}
-               onClear={text => this.SearchFilterFunction('')}
-               placeholder="Type Here..."
-               value={this.state.search}
-               />
+        return (
 
-       <FlatList
+            <View style={styles.MainContainer}>
 
-        data={ this.state.dataSource }
+                <SearchBar
+                    round
+                    searchIcon={{size: 24}}
+                    onChangeText={text => this.SearchFilterFunction(text)}
+                    onClear={text => this.SearchFilterFunction('')}
+                    placeholder="Type Here..."
+                    value={this.state.search}
+                />
 
-        ItemSeparatorComponent = {this.FlatListItemSeparator}
+                <FlatList
 
-        renderItem={({item}) =>
+                    data={this.state.dataSource}
 
-            <View style={{flex:1, flexDirection: 'row'}}>
+                    ItemSeparatorComponent={this.FlatListItemSeparator}
 
-              <Image source = {{ uri: item.image }} style={styles.imageView} />
+                    renderItem={({item}) =>
 
-              <Text onPress={() => {
+                        <View style={{flex: 1, flexDirection: 'row'}}>
 
-               /* 1. Navigate to the Details route with params */
-                this.props.navigation.navigate('Details', {
-                  isbn: item.isbn
-                });
+                            <Image source={{uri: item.image}} style={styles.imageView}/>
 
-              }}
-                style={styles.textView}>{item.title}</Text>
+                            <Text onPress={() => {
+
+
+
+                                /* 1. Navigate to the Details route with params */
+                                this.props.navigation.navigate('Details', {
+                                    isbn: item.isbn
+                                });
+
+                            }}
+                                  style={styles.textView}>{item.title}</Text>
+                        </View>
+
+                    }
+                    enableEmptySections={true}
+                    style={{marginTop: 10}}
+                    keyExtractor={(item, id) => id.toString()}
+
+                />
+
             </View>
-
-          }
-                  enableEmptySections={true}
-                  style={{ marginTop: 10 }}
-        keyExtractor={(item, id) => id.toString()}
-
-        />
-
-     </View>
-   );
- }
+        );
+    }
 }
 
 class DetailsScreen extends React.Component {
-  render() {
-    /* 2. Get the param, provide a fallback value if not available */
-    const { navigation } = this.props;
-    const isbn = navigation.getParam('isbn', 'NO-ID');
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>isbn: {JSON.stringify(isbn)}</Text>
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            tableHead: ['Available', 'Library'],
+            tableData: []
+        }
+
+        this.arrayholder = [];
+        const {navigation} = this.props;
+        this.isbn = navigation.getParam('isbn', 'NO-ID');
+
+        YellowBox.ignoreWarnings([
+            'Warning: componentWillMount is deprecated',
+            'Warning: componentWillReceiveProps is deprecated',
+        ]);
+    }
+
+    componentDidMount() {
+        return fetch(`http://192.168.0.10:4000/api/books/${this.isbn}`)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                        isLoading: false,
+                        summary: responseJson.book.summary,
+                        image: responseJson.book.image,
+                        tableData: responseJson.book.copies.map(function (copy) { return [copy.copy["checked_out?"].toString(), copy.copy.library]})
+                    },
+                    function () {
+                    }
+                );
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    render() {
+        return (
+            <ScrollView>
+            <View>
+                <Image source={{uri: this.state.image}} style={styles.imageView2}/>
+                <Text>{this.state.summary} </Text>
+                <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                    <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text}/>
+                    <Rows data={this.state.tableData} textStyle={styles.text}/>
+                </Table>
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                />
+            </View>
+        </ScrollView>
+        );
+    }
 }
 
 const RootStack = createStackNavigator(
-  {
-    Home: Metro,
-    Details: DetailsScreen,
-  },
-  {
-    initialRouteName: 'Home',
-  }
+    {
+        Home: Metro,
+        Details: DetailsScreen,
+    },
+    {
+        initialRouteName: 'Home',
+    }
 );
 
 const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
+    render() {
+        return <AppContainer/>;
+    }
 }
-
-
 
 
 const styles = StyleSheet.create({
 
-MainContainer :{
+    MainContainer: {
 
-    justifyContent: 'center',
-    flex:1,
-    margin: 5,
-    marginTop: (Platform.OS === 'ios') ? 20 : 0,
+        justifyContent: 'center',
+        flex: 1,
+        margin: 5,
+        marginTop: (Platform.OS === 'ios') ? 20 : 0,
 
-},
+    },
 
-imageView: {
+    imageView: {
 
-    width: '50%',
-    height: 100 ,
-    margin: 7,
-    borderRadius : 7
+        width: '50%',
+        height: 100,
+        margin: 7,
+        borderRadius: 7
 
-},
+    },
 
-textView: {
+    imageView2: {
 
-    width:'50%',
-    textAlignVertical:'center',
-    padding:10,
-    color: '#000'
+        width: '100%',
+        height: '50%%',
+        margin: 7,
+        borderRadius: 7
 
-}
+    },
+
+    textView: {
+
+        width: '50%',
+        textAlignVertical: 'center',
+        padding: 10,
+        color: '#000'
+
+    },
+
+    // container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+    // head: { height: 40, backgroundColor: '#f1f8ff' },
+    // text: { margin: 6 }
 
 });
